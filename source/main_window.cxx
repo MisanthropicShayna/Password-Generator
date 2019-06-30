7b76027388eb3b36510d95cb3b4055e31d725e27
Shayna Kaine <Fedorable@protonmail.ch>
2019-06-30 02:23:28 -0300

    Added comments, and revised code.
    Comments have been added. Redundant includes have been removed, shrinking the binary. Some aspects of the code were improved to be more easier to read, and a bug where more bytes than needed were being reserved when making the password generation buffer.

--- source/main_window.cxx
+++ source/main_window.cxx
@@ -12,92 +12,129 @@ int32_t MainWindow::randomNumber_(const int32_t& from, const int32_t& to) const 
 }
 
 void MainWindow::setLcdToSum_() {
-    const static std::vector<std::pair<QSpinBox*, QCheckBox*>> spinboxes {
-        {ui->spin_numericalSize, ui->check_numerical},
-        {ui->spin_specialSize, ui->check_special},
-        {ui->spin_lowerSize, ui->check_lower},
-        {ui->spin_upperSize, ui->check_upper}
-    };
+    // Calculates the total size of the password.
+    const int32_t password_size =
+        std::accumulate(charsetSelections_.begin(), charsetSelections_.end(), 0, [](const auto& acc, const auto& curr) {
+            return curr.second->isChecked() ? curr.first->value() + acc : acc;
+        });
 
-    const int32_t password_size = std::accumulate(spinboxes.begin(), spinboxes.end(), 0, [](int32_t& acc, std::pair<QSpinBox*, QCheckBox*> curr) {
-        return acc + (curr.second->isChecked() ? curr.first->value() : 0);
-    });
-
+    // Sets the lcd to dislpay the calculated size.
     ui->lcd_size->display(password_size);
 }
 
 void MainWindow::on_btn_generate_clicked() {
-    const std::size_t password_size = static_cast<std::size_t>(
-        ui->spin_numericalSize->value() + ui->spin_specialSize->value() +
-        ui->spin_upperSize->value() + ui->spin_lowerSize->value()
-    );
+    // Calculates the total password size ahead of time.
+    const int32_t password_size =
+        std::accumulate(charsetSelections_.begin(), charsetSelections_.end(), 0, [](const auto& acc, const auto& curr) {
+            return  acc + curr.first->value();
+        });
 
-    std::string generated_password;
-    generated_password.reserve(password_size);
+    /* A temporary buffer that will store the characters of the generated buffer
+     * before being written to the output textEdit. This is done so that a the dialogue
+     * does not have to re-draw and update for each character, leading to faster efficiency. */
+    QString password_generation_buffer;
 
-    for(int32_t i=0; i<ui->spin_numericalSize->value(); ++i) {
-        generated_password.push_back(numerical_[randomNumber_(0, strlen(numerical_)-1)]);
+    // As the password size is known, the required amount of bytes to store the generated password can be allocated beforehand.
+    password_generation_buffer.reserve(password_size);
+
+    /* Goes through every character set, checks if its enabled, and inserts the selected
+     * amount of characters from each character set into the password generation buffer. */
+    if(ui->check_numerical->isChecked()) {
+        for(int32_t i=0; i<ui->spin_numericalSize->value(); ++i) {
+            const int32_t& random_index = randomNumber_(0, static_cast<int32_t>(strlen(numerical_)-1));
+            const QChar& random_character = numerical_[random_index];
+            password_generation_buffer.push_back(random_character);
+        }
     }
 
-    for(int32_t i=0; i<ui->spin_specialSize->value() && ui->check_special->isChecked(); ++i) {
-        generated_password.push_back(special_[randomNumber_(0, strlen(special_)-1)]);
+    if(ui->check_special->isChecked()) {
+        for(int32_t i=0; i<ui->spin_specialSize->value(); ++i) {
+            const int32_t& random_index = randomNumber_(0, static_cast<int32_t>(strlen(numerical_)-1));
+            const QChar& random_character = special_[random_index];
+            password_generation_buffer.push_back(random_character);
+        }
     }
 
-    for(int32_t i=0; i<ui->spin_upperSize->value() && ui->check_upper->isChecked(); ++i) {
-        generated_password.push_back(upperCase_[randomNumber_(0, strlen(upperCase_)-1)]);
+    if(ui->check_special->isChecked()) {
+        for(int32_t i=0; i<ui->spin_specialSize->value(); ++i) {
+            const int32_t& random_index = randomNumber_(0, static_cast<int32_t>(strlen(upperCase_)-1));
+            const QChar& random_character = upperCase_[random_index];
+            password_generation_buffer.push_back(random_character);
+        }
     }
 
-    for(int32_t i=0; i<ui->spin_lowerSize->value() && ui->check_lower->isChecked(); ++i) {
-        generated_password.push_back(lowerCase_[randomNumber_(0, strlen(lowerCase_)-1)]);
+    if(ui->check_special->isChecked()) {
+        for(int32_t i=0; i<ui->spin_specialSize->value(); ++i) {
+            const int32_t& random_index = randomNumber_(0, static_cast<int32_t>(strlen(lowerCase_)-1));
+            const QChar& random_character = lowerCase_[random_index];
+            password_generation_buffer.push_back(random_character);
+        }
     }
 
-    std::random_shuffle(generated_password.begin(), generated_password.end());
+    // Randomly shuffles the generated password, as otherwise it would be in ordered by character set.
+    std::random_shuffle(password_generation_buffer.begin(), password_generation_buffer.end());
 
-    ui->textEdit_Output->setText(QString::fromStdString(generated_password));
+    // Writes the generated password to the output textEdit.
+    ui->textEdit_Output->setText(password_generation_buffer);
 }
 
 void MainWindow::on_btn_distribute_clicked() {
-    const static std::vector<std::pair<QSpinBox*, QCheckBox*>> spinboxes {
-        {ui->spin_numericalSize, ui->check_numerical},
-        {ui->spin_specialSize, ui->check_special},
-        {ui->spin_lowerSize, ui->check_lower},
-        {ui->spin_upperSize, ui->check_upper}
-    };
+    // The target password size. This should be the target number that is divided accross the different character sets.
+    const int32_t password_size = ui->spin_distSize->value();
 
-    const int32_t enabled = std::accumulate(spinboxes.begin(), spinboxes.end(), 0, [](int32_t& acc, std::pair<QSpinBox*, QCheckBox*> curr) {
-        return acc + curr.second->isChecked();
-    });
+    // Counts the amount of character sets that are enabled.
+    const int32_t enabled =
+        std::accumulate(charsetSelections_.begin(), charsetSelections_.end(), 0, [](const auto& acc, const auto& curr){
+            return acc + curr.second->isChecked();
+        });
 
-    if(!enabled) {
+    // Letting distribution happen with no chracter sets enabled would cause a division by zero error.
+    if(enabled == 0) {
         QMessageBox::warning(this, "Cannot distribute.", "Please enable at least one character set in order to distribute.");
         return;
     }
 
-    const int32_t division_count = static_cast<int32_t>(std::round(ui->spin_distSize->value() / enabled));
+    // Divides the target password size, by the amount of selected character sets.
+    const int32_t division = static_cast<int32_t>(std::round(password_size / enabled));
 
-    for(const auto& spinbox : spinboxes) {
-        if(spinbox.second->isChecked()) {
-            spinbox.first->setValue(division_count);
+    // Sets the occurance count of each character set to the result of the division.
+    for(const auto& charsetSelections_ : charsetSelections_) {
+        if(charsetSelections_.second->isChecked()) {
+            charsetSelections_.first->setValue(division);
         }
     }
 }
 
 void MainWindow::on_btn_genBySeed_clicked() {
+    // The target size of the password about to be generated.
+    const int32_t password_size = ui->spin_sizeBySeed->value();
+
+    // The character set / seed that will be used to randomly draw characters from.
     const QString& seed = ui->line_seed->text();
 
+    // Without a supplied seed, there would be no characters to generate a password from.
     if(seed.size() <= 0) {
         QMessageBox::warning(this, "No seed.", "Please supply a seed to generate by seed.");
         return;
     }
 
-    QString generated_password;
-    generated_password.reserve(ui->spin_sizeBySeed->value());
+    /* A temporary buffer that will store the characters of the generated buffer
+     * before being written to the output textEdit. This is done so that a the dialogue
+     * does not have to re-draw and update for each character, leading to faster efficiency. */
+    QString password_generation_buffer;
 
-    for(int32_t i=0; i<ui->spin_sizeBySeed->value(); ++i) {
-        generated_password.append(seed.at(randomNumber_(0, seed.size()-1)));
+    // As the password size is known, the required amount of bytes to store the generated password can be allocated beforehand.
+    password_generation_buffer.reserve(password_size);
+
+
+    for(int32_t i=0; i<password_size; ++i) { // The seed's total size is subtracted as the index would start from zero.
+        const int32_t random_index = randomNumber_(0, seed.size()-1);
+        const QChar& random_character = seed.at(random_index);
+        password_generation_buffer.append(random_character);
     }
 
-    ui->textEdit_Output->setText(generated_password);
+    // Sets the output textEdit's text to the generated password.
+    ui->textEdit_Output->setText(password_generation_buffer);
 }
 
 void MainWindow::on_btn_copy_clicked() {
@@ -113,18 +150,25 @@ bool MainWindow::LoadStylesheet(const std::string& qstylesheet_path) {
     std::ifstream input_stream(qstylesheet_path, std::ios::binary);
 
     if(input_stream.good()) {
-        const QString& qss_string = QString::fromStdString(std::string((std::istreambuf_iterator<char>(input_stream)), (std::istreambuf_iterator<char>())));
+        // Constructs a QString from an std::string that is being constructed with a start/end iterator of the input stream.
+        const QString& qss_string = QString::fromStdString(std::string(
+            (std::istreambuf_iterator<char>(input_stream)),
+            (std::istreambuf_iterator<char>())
+        ));
 
+        /* QCss::Parser/StyleSheet are both part of the private QtGui and QtCore libraries.
+         * There is no official public way to check the validity of a Qt StyleSheet. */
         QCss::Parser qss_parser(qss_string);
         QCss::StyleSheet stylesheet;
 
-        if(!qss_parser.parse(&stylesheet)) {
+        // If the parser returns true, the stylesheet is valid, and is applied, otherwise an error message is shown.
+        if(qss_parser.parse(&stylesheet)) {
+            setStyleSheet(qss_string);
+            return true;
+        } else {
             QMessageBox::warning(this, "QStyleSheet parsing failure.",
                 "There was an error when trying to parse style.qss, verify that the QSS is valid, or delete the file to disable styling.");
             return false;
-        } else {
-            setStyleSheet(qss_string);
-            return true;
         }
     } else {
         return false;
@@ -135,11 +179,24 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
     LoadStylesheet();
     ui->setupUi(this);
 
+    /* Pairs of character set occurance spinboxes, and their corresponding checkbox to enable/disable
+     * said character set. These are pointers, not copies.  */
+    charsetSelections_ = {
+        {ui->spin_numericalSize, ui->check_numerical},
+        {ui->spin_specialSize, ui->check_special},
+        {ui->spin_lowerSize, ui->check_lower},
+        {ui->spin_upperSize, ui->check_upper}
+    };
+
+    /* These connect the valueChanged signals from the spinboxes that count the amount of characters
+     * per character set, to update the LCD that displays the total password length. */
     connect(ui->spin_numericalSize, SIGNAL(valueChanged(int)), SLOT(setLcdToSum_()));
     connect(ui->spin_specialSize, SIGNAL(valueChanged(int)), SLOT(setLcdToSum_()));
     connect(ui->spin_upperSize, SIGNAL(valueChanged(int)), SLOT(setLcdToSum_()));
     connect(ui->spin_lowerSize, SIGNAL(valueChanged(int)), SLOT(setLcdToSum_()));
 
+    /* These connect the stateChanged signals from the checkboxes that enable a character set,
+     * to update the LCD that displays the total password length. */
     connect(ui->check_numerical, SIGNAL(stateChanged(int)), SLOT(setLcdToSum_()));
     connect(ui->check_special, SIGNAL(stateChanged(int)), SLOT(setLcdToSum_()));
     connect(ui->check_upper, SIGNAL(stateChanged(int)), SLOT(setLcdToSum_()));
--- source/main_window.hxx
+++ source/main_window.hxx
@@ -1,23 +1,27 @@
 #ifndef MAIN_WINDOW_HXX
 #define MAIN_WINDOW_HXX
 
+// Qt's Private QSS parser.
 #include <QtGui/private/qcssparser_p.h>
-#include <QMainWindow>
-#include <QClipboard>
-#include <QMessageBox>
-#include <QTextStream>
-#include <QSettings>
-#include <QFile>
-#include <QDir>
 
+// Qt Elements.
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QClipboard>
+#include <QCheckBox>
+#include <QSpinBox>
+
+// Std algorithms.
 #include <algorithm>
-#include <iostream>
-#include <sstream>
-#include <fstream>
 #include <random>
+
+// Std I/O.
+#include <iostream>
+#include <fstream>
+
+// Std containers.
 #include <string>
 #include <vector>
-#include <array>
 
 namespace Ui {
     class MainWindow;
@@ -27,25 +31,44 @@ class MainWindow : public QMainWindow {
 private:
     Q_OBJECT
     Ui::MainWindow* ui;
-    QSettings settings_;
 
+    // Constants of each character set, used several times throughout the code to generate passwords.
     static constexpr const char* upperCase_ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     static constexpr const char* lowerCase_ = "abcdefghijklmnopqrstuvwxyz";
     static constexpr const char* special_   = "!@#$%^&*()_+-=[]{}/,.";
     static constexpr const char* numerical_ = "1234567890";
 
+    // A list of spinbox/checkbox pointer pairs, used to iterate them easily, as they share related values.
+    std::vector<std::pair<QSpinBox*, QCheckBox*>> charsetSelections_;
+
+    // A simple wrapper around the standard Mersenne Twister implementation to simplify random number generation.
     int32_t randomNumber_(const int32_t&, const int32_t&) const;
 
 private slots:
+    /* Sets the LCD to the currently selected password size.
+     * This is under the slots section, as signals from other elements,
+     * such as spinboxes and checkboxes are connected to this slot, so that
+     * the LCD updates every time there is a change in character set/count. */
     void setLcdToSum_();
+
+    // Generates a password using the enabled character sets and their occurance counters.
     void on_btn_generate_clicked();
+
+    // Distributes the selected password size accross the selected character sets.
     void on_btn_distribute_clicked();
+
+    // Generates a password using the supplied seed, and writes the result to the output textEdit.
     void on_btn_genBySeed_clicked();
+
+    // Simply copies the generated password to the clipboard.
     void on_btn_copy_clicked();
 
+    // If the password size for seed based generation changes, this changes the LCD to reflect that.
     void on_spin_sizeBySeed_valueChanged(int);
 
 public:
+    /* A simple function that loads a QSS stylesheet, and is capable of handling failure by showing a messagebox.
+     * The return value indicates whether or not the stylesheet was loaded successfully. */
     bool LoadStylesheet(const std::string& = "./style.qss");
 
     explicit MainWindow(QWidget* parent = nullptr);
